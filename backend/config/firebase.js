68997b65f2a915ffef8173eaeee2907c8b812815
Yue <3571627@gmail.com>
2026-06-11 02:26:54 +0800

    fix firebase private key format for Render

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -12,7 +12,10 @@ try {
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: Buffer.from(
+          process.env.FIREBASE_PRIVATE_KEY_BASE64,
+          "base64"
+        ).toString("utf-8"),
       }),
     });
 
