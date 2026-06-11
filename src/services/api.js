933202be243dd7351fba4ecd2ad7d6649375ccab
Yue <3571627@gmail.com>
2026-06-11 11:07:35 +0800

    Demo Ready

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_BASE_URL = import.meta.env.VITE_API_URL ||   "https://phishing-lms-backend.onrender.com";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
